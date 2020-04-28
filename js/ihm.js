5634db7499395b2b5afce32ba3b22584d6952f66
Clement <gonzalle@gmail.com>
2020-04-28 23:40:39 +0200

    test 1

---
+++ js/ihm.js
@@ -0,0 +1,5 @@
+"use strict"
+let ihm = function(holderElement, imageRoot){
+    let container = document.getElementById(holderElement);
+    container.innerHTML = `<img src="${imageRoot}background-home-65.jpg" />`;
+}
